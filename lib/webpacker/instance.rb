0abf54cd34066d03b911626ba095cfa5c6c0a48f
Brian Case <bcase@usertesting.com>
2017-09-23 01:12:32 -0700

    working on webpack-dev-server

--- lib/webpacker/instance.rb
+++ lib/webpacker/instance.rb
@@ -4,6 +4,7 @@ class Webpacker::Instance
   # attr_reader :root_path, :config_path
 
   def initialize(root_path: nil, config_path: nil)
+    binding.pry
     @root_path, @config_path = root_path, config_path
   end
 
